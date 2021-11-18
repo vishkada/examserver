8f05159a228658bc1670093a00d52587fff1be1d
Vishwas <vishwas.kadam9@gmail.com>
2021-11-18 22:30:08 +0530

    Removed unwanted imports

--- src/main/java/com/exam/examserver/config/AppSecurityConfig.java
+++ src/main/java/com/exam/examserver/config/AppSecurityConfig.java
@@ -1,6 +1,5 @@
 package com.exam.examserver.config;
 
-import com.exam.examserver.service.impl.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
